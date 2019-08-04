3c516cf1adcdb3c10b409cb063a8d711939abe61
shuichiro0926 <shuichiro09263225@gmail.com>
2019-08-04 21:07:16 +0900

    fix: lintの修正

--- src/pages/DetailMenu/SelectMenuItem.tsx
+++ src/pages/DetailMenu/SelectMenuItem.tsx
@@ -1,26 +1,16 @@
 import * as React from 'react';
-import useStyles from './styles';
 
-import Input from '@material-ui/core/Input';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import FilledInput from '@material-ui/core/FilledInput';
-import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
-const SelectMenuItem = () => {
-    const classes = useStyles();
-    return (
-        <div>
-            <Select>
-                <MenuItem >アイテム</MenuItem>
-                <MenuItem >アイテム</MenuItem>
-                <MenuItem >アイテム</MenuItem>
-            </Select>
-        </div> 
-    );
-};
+const SelectMenuItem = () => (
+  <div>
+    <Select>
+      <MenuItem>アイテム</MenuItem>
+      <MenuItem>アイテム</MenuItem>
+      <MenuItem>アイテム</MenuItem>
+    </Select>
+  </div>
+);
 
 export default SelectMenuItem;
--- src/pages/DetailMenu/index.tsx
+++ src/pages/DetailMenu/index.tsx
@@ -1,91 +1,92 @@
 import * as React from 'react';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Button from '@material-ui/core/Button';
 import useStyles from './styles';
 
 import RadioMenuItem from './RadioMenuItem';
 import SelectMenuItem from './SelectMenuItem';
 
-import RadioGroup from '@material-ui/core/RadioGroup';
-import Button from '@material-ui/core/Button';
 
 const thumbnail = require('images/img_ad1aa8fefac6d770b0441fefbfceb61b917960.jpg');
 
 const DetailMenu = () => {
-    const classes = useStyles();
-    return (
-        <div>
-            <img style={{height: '160px'}} src={`${thumbnail}`} />
+  const classes = useStyles();
 
-            <div className={classes.menuArea}>
-                <div className={classes.menuTitleArea}>
-                    <div className={classes.titleLetter}>Gong cha Spetial</div>
-                    <div className={classes.subTitleLetter}>ゴンチャ　スペシャル</div>
-                </div>
+  return (
+    <div>
+      <img style={{ height: '160px' }} src={`${thumbnail}`} alt="" />
 
-                <div className={classes.baseTeaArea}>
-                    <div className={classes.titleLetter}>ベースティー</div>
-                        <RadioGroup aria-label="gender" name="gender1">
+      <div className={classes.menuArea}>
+        <div className={classes.menuTitleArea}>
+          <div className={classes.titleLetter}>Gong cha Spetial</div>
+          <div className={classes.subTitleLetter}>ゴンチャ スペシャル</div>
+        </div>
 
-                        <RadioMenuItem />
-                        <RadioMenuItem />
-                        <RadioMenuItem />
-                        <RadioMenuItem />
+        <div className={classes.baseTeaArea}>
+          <div className={classes.titleLetter}>ベースティー</div>
+          <RadioGroup aria-label="gender" name="gender1">
 
-                        </RadioGroup>
+            <RadioMenuItem />
+            <RadioMenuItem />
+            <RadioMenuItem />
+            <RadioMenuItem />
 
-                </div>        
+          </RadioGroup>
 
-                <div className={classes.selectArea}>
-                <div>
-                        <div style={{display: 'inline-block', width: '141px'}}>
-                            <div className={classes.titleLetter}>ICE/HOT</div>
-                            <SelectMenuItem />
-                        </div>
-                        <div style={{display: 'inline-block', width: '141px'}}>
-                            <div className={classes.titleLetter}>サイズ</div>
-                            <SelectMenuItem />
-                        </div>
-                    </div>
+        </div>
 
-                    <div>
-                        <div style={{display: 'inline-block', width: '141px'}}>
-                            <div className={classes.titleLetter}>甘さ</div>
-                            <SelectMenuItem />
-                        </div>
-                        <div style={{display: 'inline-block', width: '141px'}}>
-                            <div className={classes.titleLetter}>氷の量</div>
-                            <SelectMenuItem />
-                        </div>
-                    </div>
-                </div>
-
-            <div className={classes.toppingArea}>
-            <div className={classes.titleLetter}>トッピング（最大３つまで）</div>
-            <RadioGroup aria-label="gender" name="gender1">
-                <RadioMenuItem />
-                <RadioMenuItem />
-                <RadioMenuItem />
-                <RadioMenuItem />
-                <RadioMenuItem />
-                <RadioMenuItem />
-            </RadioGroup>
-
+        <div className={classes.selectArea}>
+          <div>
+            <div style={{ display: 'inline-block', width: '141px' }}>
+              <div className={classes.titleLetter}>ICE/HOT</div>
+              <SelectMenuItem />
             </div>
-        
-            <div className={classes.numArea}>
-                <div className={classes.titleLetter}>個数</div>
-                <SelectMenuItem />
+            <div style={{ display: 'inline-block', width: '141px' }}>
+              <div className={classes.titleLetter}>サイズ</div>
+              <SelectMenuItem />
             </div>
-            
+          </div>
+
+          <div>
+            <div style={{ display: 'inline-block', width: '141px' }}>
+              <div className={classes.titleLetter}>甘さ</div>
+              <SelectMenuItem />
+            </div>
+            <div style={{ display: 'inline-block', width: '141px' }}>
+              <div className={classes.titleLetter}>氷の量</div>
+              <SelectMenuItem />
+            </div>
+          </div>
         </div>
 
-        <div className={classes.buttonArea}>
-
-            <Button variant="contained">注文内容を確認する</Button>
+        <div className={classes.toppingArea}>
+          <div className={classes.titleLetter}>トッピング（最大３つまで）</div>
+          <RadioGroup aria-label="gender" name="gender1">
+            <RadioMenuItem />
+            <RadioMenuItem />
+            <RadioMenuItem />
+            <RadioMenuItem />
+            <RadioMenuItem />
+            <RadioMenuItem />
+          </RadioGroup>
 
         </div>
-            
+
+        <div className={classes.numArea}>
+          <div className={classes.titleLetter}>個数</div>
+          <SelectMenuItem />
         </div>
-    );
+
+      </div>
+
+      <div className={classes.buttonArea}>
+
+        <Button variant="contained">注文内容を確認する</Button>
+
+      </div>
+
+    </div>
+  );
 };
 
 export default DetailMenu;
