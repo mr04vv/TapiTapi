f595d4398e1ec971c2dc326dc7e7332a48f34d0c
mr04vv <mr04vv@gmail.com>
2019-08-08 02:25:03 +0900

    add: generating qr

---
+++ src/hooks/Menu/useOrderedItem.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import useReactRouter from 'use-react-router';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+const useOrderedItem = () => {
+  const { history } = useReactRouter();
+  const { match } = useReactRouter();
+  const { params }: any = match;
+  const [infoId, setInfoId] = useState<string>();
+
+  useEffect(() => {
+    setInfoId(params.item);
+  }, []);
+
+  const db = firebase.firestore();
+  db.collection('orderList').doc(params.item)
+    .onSnapshot((doc) => {
+      const data = doc.data();
+      if (data!.isRead) {
+        history.push({
+          pathname: '/paid',
+        });
+      }
+    });
+  return {
+    infoId,
+  };
+};
+export default useOrderedItem;
