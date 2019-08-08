61cc2c56b4122b6e856c69f0ed3f7cc67621f29e
mr04vv <mr04vv@gmail.com>
2019-08-08 21:44:50 +0900

    fix: tsx -> jsx use google map component

--- src/pages/GeoSearch/Map/index.jsx
+++ src/pages/GeoSearch/Map/index.jsx
@@ -1,36 +1,26 @@
 /* eslint-disable no-console */
 import * as React from 'react';
 import {
-  GoogleApiWrapper, Map, Marker, GoogleAPI,
+  GoogleApiWrapper, Map, Marker,
 } from 'google-maps-react';
-import StoreInterface from 'interfaces/StoreInterface';
 
-interface PropsInterface {
-  google: GoogleAPI;
-  defaultLocation: Position | undefined;
-  loaded: boolean;
-  storeList: StoreInterface[];
-  setIsShowModal: (v: StoreInterface | false) => void;
-}
-
-const CustomMap = (props: PropsInterface) => {
+const CustomMap = (props) => {
   const {
     defaultLocation, google, storeList, setIsShowModal,
   } = props;
   return (
-    defaultLocation!
+    defaultLocation
       ? (
         <Map
           google={google}
           zoom={14}
           style={{ height: 'calc(100vh - 56px)' }}
-          initialCenter={{ lat: defaultLocation!.coords.latitude, lng: defaultLocation!.coords.longitude }}
+          initialCenter={{ lat: defaultLocation.coords.latitude, lng: defaultLocation.coords.longitude }}
         >
-          {storeList.map((s: StoreInterface) => (
+          {storeList.map(s => (
             <Marker
               title={s.storeName}
               position={{ lat: s.lat, lng: s.lng }}
-              // eslint-disable-next-line no-console
               onClick={() => setIsShowModal(s)}
             />
           ))}
